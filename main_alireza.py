1df559be4da97036d3ec1d2e0afc838a444baf12
alirezatabatabeian <alireza2541@aut.ac.ir>
2022-06-30 16:19:15 +0430

    Generating Sudoku_test 2

--- main_alireza.py
+++ main_alireza.py
@@ -1,70 +1,18 @@
-# import pygame
-# import time
-# import dokusan
-# pygame.font.init()
-
+import numpy as np
 from dokusan import generators, renderers, solvers
 from dokusan.boards import BoxSize, Sudoku
-import numpy as np
 
-sudoku = generators.random_sudoku(avg_rank=150)
-# print(sudoku)
+sudoku = generators.random_sudoku(avg_rank=150)  # Generate a Sudoku
+print(renderers.colorful(sudoku))  # Print Sudoku
 
-sudoku_np_array = np.array(list(str(sudoku))).reshape(9, 9)
-sudoku_list = 
-# print(sudoku_np_array)
+solution = solvers.backtrack(sudoku)  # Solve a Sudoku
+print(renderers.colorful(solution))  # Print Solved Sudoku
 
-# print(list(sudoku_np_array))
+sudoku_np_array = np.array(list(str(sudoku)), dtype=int).reshape(9, 9)  # Make np_array
 
-temp = Sudoku.from_list(
-    list(sudoku_np_array),
-    box_size=BoxSize(3, 3),
+sudoku.update(  # Update Sudoku
+    Sudoku.from_list(
+        sudoku_np_array.tolist(),
+        box_size=BoxSize(3, 3),
+    ).cells()
 )
-
-sudoku.update(temp.cells())
-print(sudoku)
-
-# print(renderers.colorful(sudoku))
-
-# print(sudoku.is_valid())
-
-# sudoku = Sudoku.from_list(
-#     [
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 3, 0, 8, 5],
-#         [0, 0, 1, 0, 2, 0, 0, 0, 0],
-#         [0, 0, 0, 5, 0, 7, 0, 0, 0],
-#         [0, 0, 4, 0, 0, 0, 1, 0, 0],
-#         [0, 9, 0, 0, 0, 0, 0, 0, 0],
-#         [5, 0, 0, 0, 0, 0, 0, 7, 3],
-#         [0, 0, 2, 0, 1, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 4, 0, 0, 0, 9],
-#     ],
-#     box_size=BoxSize(3, 3),
-# )
-
-# sudoku1 = Sudoku.from_list(
-#     [
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 3, 0, 8, 5],
-#         [0, 0, 1, 0, 2, 0, 0, 0, 0],
-#         [0, 0, 0, 5, 0, 7, 0, 0, 0],
-#         [0, 0, 4, 0, 0, 0, 1, 0, 0],
-#         [0, 9, 0, 0, 0, 0, 0, 0, 0],
-#         [5, 0, 0, 0, 0, 0, 0, 7, 3],
-#         [0, 0, 2, 0, 1, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 4, 0, 0, 0, 9],
-#     ],
-#     box_size=BoxSize(3, 3),
-# )
-
-# sudoku2 = Sudoku.from_string("100080000000100030500000092003098000005070243040000000900000706000009408060002000", box_size=BoxSize(3, 3))
-# print(renderers.colorful(sudoku2))
-
-# sudoku.update(sudoku1.cells())
-# solution = solvers.backtrack(sudoku)
-
-# print(renderers.colorful(sudoku))
-# print(sudoku.)
-
-# print(renderers.colorful(solution))
